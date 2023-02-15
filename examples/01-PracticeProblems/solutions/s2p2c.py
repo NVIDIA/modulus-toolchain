732ca09d2460da2d8b2c2ae3d5a4d1385cf3349c
Mohammad <mnabian@nvidia.com>
2023-02-15 10:15:39 -0800

    formatting

--- examples/01-PracticeProblems/solutions/s2p2c.py
+++ examples/01-PracticeProblems/solutions/s2p2c.py
@@ -1,16 +1,18 @@
 from cfg import *
 
-[x, y], [u, v, pp] = p.add_neural_network(name="NN", inputs=["x", "y"], outputs=["u", "v", "p"])
+[x, y], [u, v, pp] = p.add_neural_network(
+    name="NN", inputs=["x", "y"], outputs=["u", "v", "p"]
+)
 
-w,h = 2,1
-r1 = p.Rectangle("r1", (0,0), (w,h))
-ch1 = p.Channel2D("ch1", (0,0), (w,h))
+w, h = 2, 1
+r1 = p.Rectangle("r1", (0, 0), (w, h))
+ch1 = p.Channel2D("ch1", (0, 0), (w, h))
 
-subr = p.Rectangle("subr1", (w/2-.1,0), (w/2+.1,h*.8))
+subr = p.Rectangle("subr1", (w / 2 - 0.1, 0), (w / 2 + 0.1, h * 0.8))
 ch1 = p.GeometryDifference("gd", ch1, subr)
 
-inlet = p.add_boundary_subdomain("inlet", geom=r1, criteria=Eq(x,0))
-outlet = p.add_boundary_subdomain("outlet", geom=r1, criteria=Eq(x,w))
+inlet = p.add_boundary_subdomain("inlet", geom=r1, criteria=Eq(x, 0))
+outlet = p.add_boundary_subdomain("outlet", geom=r1, criteria=Eq(x, w))
 
 r_interior = p.add_interior_subdomain("r_interior", geom=r1)
 ch_interior = p.add_interior_subdomain("ch_interior", geom=ch1)
@@ -18,31 +20,49 @@ ch_interior = p.add_interior_subdomain("ch_interior", geom=ch1)
 noslip = p.add_boundary_subdomain("no_slip", geom=ch1)
 
 # p.add_constraint("c1", enforce(equation=Eq(x,0), on_domain=r_interior))
-p.add_constraint("inlet_u", enforce(equation=Eq(u,1), on_domain=inlet))
-p.add_constraint("inlet_v", enforce(equation=Eq(v,0), on_domain=inlet))
+p.add_constraint("inlet_u", enforce(equation=Eq(u, 1), on_domain=inlet))
+p.add_constraint("inlet_v", enforce(equation=Eq(v, 0), on_domain=inlet))
 
-p.add_constraint("outlet_p", enforce(equation=Eq(pp,0), on_domain=outlet))
+p.add_constraint("outlet_p", enforce(equation=Eq(pp, 0), on_domain=outlet))
 
-p.add_constraint("noslip_u", enforce(equation=Eq(u,0), on_domain=noslip))
-p.add_constraint("noslip_v", enforce(equation=Eq(v,0), on_domain=noslip))
+p.add_constraint("noslip_u", enforce(equation=Eq(u, 0), on_domain=noslip))
+p.add_constraint("noslip_v", enforce(equation=Eq(v, 0), on_domain=noslip))
 
 nu = 0.02
-p.add_constraint("continuity", enforce(equation=Eq(u.diff(x)+v.diff(y),0), 
-                                       on_domain=ch_interior))
+p.add_constraint(
+    "continuity", enforce(equation=Eq(u.diff(x) + v.diff(y), 0), on_domain=ch_interior)
+)
 
-p.add_constraint("momentum_x", enforce(equation=Eq(u*u.diff(x)+v.diff(y)+pp.diff(x),
-                                                   nu*(u.diff(x,x) + u.diff(y,y))), 
-                                       on_domain=ch_interior))
+p.add_constraint(
+    "momentum_x",
+    enforce(
+        equation=Eq(
+            u * u.diff(x) + v.diff(y) + pp.diff(x), nu * (u.diff(x, x) + u.diff(y, y))
+        ),
+        on_domain=ch_interior,
+    ),
+)
 
-p.add_constraint("momentum_y", enforce(equation=Eq(u*u.diff(x)+v.diff(y)+pp.diff(y),
-                                                   nu*(v.diff(x,x) + v.diff(y,y))), 
-                                       on_domain=ch_interior))
+p.add_constraint(
+    "momentum_y",
+    enforce(
+        equation=Eq(
+            u * u.diff(x) + v.diff(y) + pp.diff(y), nu * (v.diff(x, x) + v.diff(y, y))
+        ),
+        on_domain=ch_interior,
+    ),
+)
 
 # Adding mass balance
 import sympy as sp
+
 nx = sp.Symbol("normal_x")
 ny = sp.Symbol("normal_y")
 
-inlet_outlet = p.add_boundary_subdomain("inlet_outlet", geom=r1, criteria=Eq(x,0) | Eq(x,w))
-p.add_constraint("mass_balance", enforce(equation=Eq(Integral(nx*u+ny*v, x,y),0), 
-                                         on_domain=inlet_outlet))
+inlet_outlet = p.add_boundary_subdomain(
+    "inlet_outlet", geom=r1, criteria=Eq(x, 0) | Eq(x, w)
+)
+p.add_constraint(
+    "mass_balance",
+    enforce(equation=Eq(Integral(nx * u + ny * v, x, y), 0), on_domain=inlet_outlet),
+)
--- examples/04-WaveEquationPINNs/wave_1d_problem.py
+++ examples/04-WaveEquationPINNs/wave_1d_problem.py
@@ -10,28 +10,25 @@ import numpy as np
 )
 
 L = float(np.pi)
-geo = p.Line1D("geom", 0,L)
+geo = p.Line1D("geom", 0, L)
 
-interior = p.add_interior_subdomain("interior", 
-                                    geom=geo, 
-                                    params={t:(0,2*L)})
+interior = p.add_interior_subdomain("interior", geom=geo, params={t: (0, 2 * L)})
 
-initial_t0 = p.add_interior_subdomain("initial_t0", 
-                                    geom=geo, 
-                                    params={t:(0,0)})
+initial_t0 = p.add_interior_subdomain("initial_t0", geom=geo, params={t: (0, 0)})
 
-boundary = p.add_boundary_subdomain("boundary", 
-                                    geom=geo, 
-                                    params={t:(0,2*L)})
+boundary = p.add_boundary_subdomain("boundary", geom=geo, params={t: (0, 2 * L)})
 
 
 c = 1.0
 wave_eq = Eq(u.diff(t, 2), (c**2 * u.diff(x)).diff(x))
 
 from sympy import sin
-p.set_constraints({
-    "initial_u": enforce(equation=Eq(u, sin(x)), on_domain=initial_t0),
-    "initial_u_t": enforce(equation=Eq(u.diff(t), sin(x)), on_domain=initial_t0),
-    "boundary": enforce(equation=Eq(u,0), on_domain=boundary),
-    "wave_equation": enforce(equation=wave_eq, on_domain=interior)
-})
+
+p.set_constraints(
+    {
+        "initial_u": enforce(equation=Eq(u, sin(x)), on_domain=initial_t0),
+        "initial_u_t": enforce(equation=Eq(u.diff(t), sin(x)), on_domain=initial_t0),
+        "boundary": enforce(equation=Eq(u, 0), on_domain=boundary),
+        "wave_equation": enforce(equation=wave_eq, on_domain=interior),
+    }
+)
